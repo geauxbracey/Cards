865d6c6c6ce815aff23f0d06dcf897f18d3dc00b
Kaitlyn <braceykaitlyn@gmail.com>
2024-10-01 13:22:33 -0400

    Initial Commit

---
+++ Cards/CardsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CardsApp.swift
+//  Cards
+//
+//  Created by Kaitlyn Bracey on 10/1/24.
+//
+
+import SwiftUI
+
+@main
+struct CardsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
